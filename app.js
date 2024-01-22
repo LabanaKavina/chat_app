dfb59050f9b021d37f154c8cad06b1d125894663
kavina <kavina@avestatechnologies.com>
2024-01-22 12:11:21 +0530

    socket add

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require('express')
+const socket = require('socket.io')
 const app = express()
+const http = require('http');
+const server = http.createServer(app);
+const io = socket(server)
 
-app.listen(6000)
+
+io.on('connection',(socket)=>{
+
+console.log("user connected");
+
+socket.on('disconnect',()=>{
+    console.log("user disconnected");
+})
+
+socket.on('chat', (msg)=>{
+    io.emit("chat",msg)
+    console.log(msg);
+})
+
+})
+
+
+
+server.listen(6000,()=>{
+    console.log("server listening on port 6000")
+})
